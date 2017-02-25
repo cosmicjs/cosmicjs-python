902d621054de451812284be8c4c6782066117d0e
Pavel Uskavan <uskavan@gmail.com>
2017-02-25 14:46:48 +0200

    DELETE

--- tests/test_add_object.py
+++
@@ -1,8 +0,0 @@
-from pythoncosmicjs import Api
-
-bucket_slug = 'pythoncosmicjs'
-read_key = '1eEggaao4xJFBnhAOBGV6oX40chL7Kp71bMXpyCMk51OUt9acb'
-write_key = '9rIG8XYnKDP60MtIis8ORv1pzgp2kYsT7PsXyV1yciqwaNIpPC'
-api = Api(bucket_slug, read_key, write_key)
-
-print(api.add_object('Test add object', 'Test add object'))
