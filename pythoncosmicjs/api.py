2544e30ad2cc175617fa8d72c6fafcb77c189770
Tony Spiro <tspiro@tonyspiro.com>
2017-03-18 11:36:26 -0500

    Update api.py

--- pythoncosmicjs/api.py
+++ pythoncosmicjs/api.py
@@ -2,23 +2,23 @@ import requests
 
 
 class Api(object):
-    def __init__(self, buckets, read_key=None, write_key=None, base_url=None):
-        self.buckets = buckets
+    def __init__(self, bucket, read_key=None, write_key=None, base_url=None):
+        self.bucket = bucket
         self.base_url = 'https://api.cosmicjs.com/v1' or base_url
         self.read_key = '?read_key=%s' % read_key if read_key else ''
         self.write_key = '?write_key=%s' % write_key if write_key else ''
 
     def all_content(self):
-        url = '%s/%s/' % (self.base_url, self.buckets)
+        url = '%s/%s/' % (self.base_url, self.bucket)
         return self.deserialization(url)
 
     def list_objects(self, limit=None, skip=None):
         payload = {'limit': limit, 'skip': skip}
-        url = '%s/%s/objects%s' % (self.base_url, self.buckets, self.read_key)
+        url = '%s/%s/objects%s' % (self.base_url, self.bucket, self.read_key)
         return self.deserialization(url, payload)
 
     def one_object(self, object_slug):
-        url = '%s/%s/object/%s/%s' % (self.base_url, self.buckets, object_slug, self.read_key)
+        url = '%s/%s/object/%s/%s' % (self.base_url, self.bucket, object_slug, self.read_key)
         return self.deserialization(url)
 
     def list_media(self, limit=None, skip=None):
@@ -26,27 +26,27 @@ class Api(object):
             query_parameters = '?limit=%s&?skip=%s' % (limit, skip)
         else:
             query_parameters = '%s%s' % ('?limit=' + str(limit) if limit else '', '?skip=' + str(skip) if skip else '')
-        url = '%s/%s/media%s%s' % (self.base_url, self.buckets, self.read_key, query_parameters)
+        url = '%s/%s/media%s%s' % (self.base_url, self.bucket, self.read_key, query_parameters)
         return self.deserialization(url)
 
     def search_object(self, object_type=None, limit=None, skip=None, metafield_keys=None, metafield_value=None):
         payload = {'limit': limit, 'skip': skip, 'metafield_key': metafield_keys, 'metafield_value': metafield_value}
-        url = '%s/%s/object-type/%s/search' % (self.base_url, self.buckets, object_type)
+        url = '%s/%s/object-type/%s/search' % (self.base_url, self.bucket, object_type)
         return self.deserialization(url, payload)
 
     def add_object(self, title, content):
-        url = '%s/%s/add-object%s' % (self.base_url, self.buckets, self.write_key)
+        url = '%s/%s/add-object%s' % (self.base_url, self.bucket, self.write_key)
         r = requests.post(url, data={'title': title, 'type_slug': title.replace(' ', '-'), 'content': content,
                                      'write_key': self.write_key})
         return r.json()
 
     def edit_object(self, slug, title, content):
-        url = '%s/%s/edit-object' % (self.base_url, self.buckets)
+        url = '%s/%s/edit-object' % (self.base_url, self.bucket)
         r = requests.put(url, data={'slug': slug, 'title': title, 'content': content})
         return r.json()
 
     def delete_object(self, object_slug):
-        url = '%s/%s/%s' % (self.base_url, self.buckets, object_slug)
+        url = '%s/%s/%s' % (self.base_url, self.bucket, object_slug)
         r = requests.delete(url, data={'write_key': self.write_key})
         return r.json()
 
